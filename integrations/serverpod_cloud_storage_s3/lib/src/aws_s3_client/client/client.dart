9c891e40839d05d8b151137ba7f63effd90eb77a
mathis6787 <mathisdesmeules@protonmail.com>
2025-11-12 00:35:29 -0500

    feat: Add logging for HEAD requests in AwsS3Client and S3CloudStorage

--- integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_client/client/client.dart
+++ integrations/serverpod_cloud_storage_s3/lib/src/aws_s3_client/client/client.dart
@@ -68,6 +68,8 @@ class AwsS3Client {
   }
 
   Future<Response> headObject(String key) {
+    print('Key to HEAD: $key');
+    print('🧭 [AwsS3Client] HEAD request → http://$_host/$_bucketId/$key');
     return _doSignedHeadRequest(key: key);
   }
 
